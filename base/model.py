2d8cfbe5a9171c10b5e0b88ed278502aa9345ebe
muyi <kang.yang.7muyi@gmail.com>
2026-04-07 16:09:22 +0800

    FEAT: Remove encode method in  BaseMolde

--- base/model.py
+++ base/model.py
@@ -3,9 +3,6 @@ import torch.nn as nn
 
 
 class BaseModel(nn.Module):
-    def encode(self, input_ids: torch.Tensor, attention_mask: torch.Tensor) -> dict[str, torch.Tensor]:
-        raise NotImplementedError()
-
     def encode_qry(self, input_ids: torch.Tensor, attention_mask: torch.Tensor) -> dict[str, torch.Tensor]:
         raise NotImplementedError()
 
