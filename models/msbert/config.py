facaf302288faa11467eee61f9587de5db98ff41
muyi <kang.yang.7muyi@gmail.com>
2026-04-07 20:00:06 +0800

    Add **MSBert** model
    New: models/msbert/*

---
+++ models/msbert/config.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+    # Model
+    pretrained_model: str = "bert-base-uncased"
+    n_heads: int = 8
+    attn_dim: int = 128
+    out_dim: int = 128
+    qry_span_size: int = 4
+    doc_span_size: int = 4
+    dropout: float = 0.1
+
+    # Tokenizer
+    qry_maxlen: int = 32
+    doc_maxlen: int = 256
+    # qry_span_size: int = 4
+    # doc_span_size: int = 4
+
+    # Training
+    dataset_path: str = ""
+    checkpoint_path: str = ""
+
+    amp: bool = True
+    epoch: int = 5
+    bsize: int = 128
+    accumulation_steps: int = 2
+    warmup: int = 1000
+    lr_backbone: float = 3e-6
+    lr_other: float = 1e-5
+    lr_min_ratio: float = 0.1
+
+    log_interval: int = 25
+
+    temperature: float = 0.1
